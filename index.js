d1413a58b69f1150d7c1b9c34796a143783824ba
Taylor Wright <tjwri25@gmail.com>
2021-03-27 13:53:21 -0600

    inquirer, addemp func, view all func

--- index.js
+++ index.js
@@ -3,114 +3,200 @@ const inquirer = require('inquirer');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-  
-   
+
+
     port: 3306,
-  
-    
+
+
     user: 'root',
-  
-    
-    password: 'root',
+
+
+    password: 'Kross!0325',
     database: 'employeeDB',
 });
 
 connection.connect((err) => {
     if (err) throw err;
-    runSearch();
+    console.log("\n 𝔼𝕞𝕡𝕝𝕠𝕪𝕖𝕖 𝕋𝕣𝕒𝕔𝕜𝕖𝕣 \n");
+    mainMenu();
 });
 
-console.log("\n 𝔼𝕞𝕡𝕝𝕠𝕪𝕖𝕖 𝕋𝕣𝕒𝕔𝕜𝕖𝕣 \n");
-    StartUp();
 
-    function StartUp(){
-        inquirer
+function mainMenu() {
+    inquirer
         .prompt({
-          name: "action",
-          type: "list",
-          message: "MAIN MENU",
-          choices: [
-            "View all employees",
-            "View all employees by role",
-            "View all employees by department",
-            "View all employees by manager",
-            "Add employee",
-            "Add role",
-            "Add department",
-            "Update employee role",
-            "Update employee manager",
-           
-          ]
-    })
+            name: "action",
+            type: "list",
+            message: "MAIN MENU",
+            choices: [
+                "View all employees",
+                "View all employees by role",
+                "View all employees by department",
+                "View all employees by manager",
+                "Add employee",
+                "Add role",
+                "Add department",
+                "Update employee role",
+                "Update employee manager",
 
-.then((answer) => {
-    switch (answer.action) {
-        case "View all employees":
-            allEmp();
-            break;
+            ],
+        })
+        .then((answer) => {
+            switch (answer.action) {
+                case "View all employees":
+                    allEmp();
+                    // allEmp () is replaced with "Dan"
+                    break;
 
-        case "View all employees by role":
-            allRole();
-            break;
-        case "View all employees by department":
-             allDept();
-             break;
-        case "View all employees by manager":
-             allMngr();
-            break;
-        case "Add employee":
-            addEmp();
-            break;
-        case "Add role":
-            addRole();
-            break;
-        case "Add department":
-            addDept();
-            break;
-        case "Update employee role":
-            updateEmpRole();
-            break;
-        case "Update employee manager":
-            updateEmpMngr();
-            break;
-        case 'Exit':
-            connection.end();
-            break;
+                case "View all employees by role":
+                    allRole();
+                    break;
+                case "View all employees by department":
+                    allDept();
+                    break;
+                case "View all employees by manager":
+                    allMngr();
+                    break;
+                case "Add employee":
+                    addEmp();
+                    break;
+                case "Add role":
+                    addRole();
+                    break;
+                case "Add department":
+                    addDept();
+                    break;
+                case "Update employee role":
+                    updateEmpRole();
+                    break;
+                case "Update employee manager":
+                    updateEmpMngr();
+                    break;
+                case 'Exit':
+                    connection.end();
+                    break;
 
-        default:
-            console.log(`Invalid action: ${answer.action}`);
-            break;
-        }
-    });
+            }
+        });
 };
+// view all employee function 
+const allEmp = () => {
 
-const allEmp(){
 
+    const query = "SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, concat(m.first_name, ' ' ,  m.last_name) AS manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY ID ASC";
+    connection.query(query, function (err, data) {
+        if (err) throw err;
+        console.table(data);
+        mainMenu();
+    });
 }
+// view all employees by department function
+const allDept = () => {
+    const query = "SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;";
+    connection.query(query, function (err, data) {
+        if (err) throw err;
+        console.table(data);
+        mainMenu();
+    });
 
-const allDept(){
-}
+},
 
-const allRole(){
+    // view all employees by role function 
+allRole = () => {
+        const query = "SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;";
+        connection.query(query, function (err, data) {
+            if (err) throw err;
+            console.table(data);
+            mainMenu();
+        });
 
-}
+    },
+    addEmp = () => {
+        connection.query("SELECT * FROM role", function (err, res) {
+            if (err) throw err;
+    
+            inquirer
+                .prompt([
+                    {
+                        name: "first_name",
+                        type: "input",
+                        message: "What is the employee's first name? ",
+                    },
+                    {
+                        name: "last_name",
+                        type: "input",
+                        message: "What is the employee's last name? "
+                    },
+                    {
+                        name: "role",
+                        type: "list",
+                        choices: function () {
+                            var roleList = [];
+                            for (let i = 0; i < res.length; i++) {
+                                roleList.push(res[i].title);
+                            }
+                            return roleList;
+                        },
+                        message: "What is the employee's role? "
+                    },
+                    {
+                        type: "input",
+                        message: "What is the manager id number?",
+                        name: "managerID"
+                      }
+    
+                ]).then(function (answer) {
+                    let roleID;
+                    for (let x = 0; x < res.length; x++) {
+                        if (res[x].title == answer.role) {
+                            roleID = res[x].id;
+                            console.log(roleID)
+                        }
+    
+                    }
+                    let managerID;
+                    for (let k = 0; k < res.length; k++) {
+                        if (res[k].title == answer.manager) {
+                            managerID = res[k].id;
+                            console.log(managerID)
+                        }
+                    }
+    
+    
+                    connection.query(
+                        "INSERT INTO employee SET ?",
+                        {
+                            first_name: answer.first_name,
+                            last_name: answer.last_name,
+                            role_id: roleID,
+                            manager_id: answer.managerID
+                        },
+                        function (err) {
+                            if (err) throw err;
+                            console.log(`Successfully added ${answer.first_name} ${answer.last_name} to Employees`);
+                            mainMenu();
+                        }
+                    )
+                })
+        })
+    }
+              
+// }
 
-const addEmp(){
+// const addRole(){
 
-}
+// }
 
-const addRole(){
+// const addDept(){
 
-}
+// }
 
-const addDept(){
+// const updateEmpRole(){
 
-}
+// }
 
-const updateEmpRole(){
+// const updateEmpMngr(){
 
-}
+// }
 
-const updateEmpMngr(){
 
-}
---
+++ seed.sql
@@ -0,0 +1,107 @@
+DROP DATABASE IF EXISTS employeeDB;
+
+CREATE DATABASE employeeDB;
+
+USE employeeDB;
+
+----- Create Department table -----
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+----- Create Role Table -----
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
+----- Create Employee Table -----
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
+);
+
+INSERT INTO department (id, name)
+VALUES (1, "Parks Department");
+
+INSERT INTO department (id, name)
+VALUES (2, "City Department");
+
+INSERT INTO department (id, name)
+VALUES (3, "City Council");
+----------
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, "Director", 120000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (2, "Deputy Director", 90000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (3, "Assistant", 35000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (4, "Administrator", 65000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (5, "Office Manager", 75000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (6, "City Manager", 160000, 2);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (7, "Assistant City Manager", 135000, 2);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (8, "City Council", 52000, 3);
+
+----------
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (8, "Chris", "Traeger", 6, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Ron", "Swanson", 1, 8);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (2, "Leslie", "Knope", 2, 1);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (3, "April", "Ludgate-Dwyer", 3, 1);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (4, "Andrew", "Dwyer", 3, 2);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (5, "Tom", "Haverford", 4, 1);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (6, "Gary", "Gergich", 4, 1);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (7, "Donna", "Meagle", 5, 1);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (9, "Ben", "Wyatt", 7, 8);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (10, "Jeremy", "Jamm", 8, 8);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (11, "Bill", "Dexhart", 8, 8);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (12, "Douglass", "Houser", 8, 8);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (13, "Fielding", "Milton", 8, 8);
